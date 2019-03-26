9dd28cef8c197b4809115cf2dd52f6746e636277
Seungwoo Kim <seungwookim08@gmail.com>
2019-03-25 23:07:21 -0400

    fix bytes-code free

--- source/main.py
+++ source/main.py
@@ -7,16 +7,21 @@ def build_model():
     while True:
         print("/train/train-ham-{:05d}.txt".format(i))
         try:
-            with open("./train/train-ham-{:05d}.txt".format(i),'r') as test_file:
+            with open(".\\train\\train-ham-{:05d}.txt".format(i), encoding='utf-8') as test_file:
                 txt = test_file.read()
                 tokenized = re.split('\[\^a-zA-Z\]',txt)
                 print(tokenized)
         except FileNotFoundError:
             print('Build model for ham is done')
             break
+        except UnicodeDecodeError:
+            # utf-8 code can't decode, then try with byte code
+            with open(".\\train\\train-ham-{:05d}.txt".format(i), 'rb') as test_file:
+                txt = test_file.read()
+                print(txt)
         i += 1
 
-def __main__():
+def __main__():    
     is_build = input('Do you want to build a model? (Y/N)')
     if is_build.lower() == 'y':
         build_model()
